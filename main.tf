777bfda760d1f5a8b2fbe80c30e5f3f05aa7c72e
imsindhi <98776768+imsindhi@users.noreply.github.com>
2022-09-20 11:50:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.23.0"
+      source  = "hashicorp/azurerm"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "terraform-demo"
+    storage_account_name = "terraformdemostorage9105"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
 
-provider "azurerm" {
-  # Configuration options
+
+module "StorageAccount" {
+  source = "./StorageAccount"
+  base_name = "TerraformExample01"
+  resource_group_name = "terraform-demo"
+  location = "West US"
 }
